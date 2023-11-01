819ae17af2f4ab735368ff2ecd99713c5f0c6823
Megan Ring <meganbethring@gmail.com>
2023-11-01 15:56:40 -0700

    Center name card within #home

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,30 +1,40 @@
 import React from 'react';
 import * as Theme from '@theme';
+import { UserProvider, useUser } from '@providers';
 
 /* TODO:
-1. Center Card component within the #home.
 2. Display user's full name and their email. Use UserProvider's functionality (see providers.js)
 3. Display user's masked phone number, and implement a way to unmask it. Use UserProvider's context.
 */
-
 const Card = () => {
+  const user = React.useContext(useUser)
+
   return (
     <div id="card">
-      ...
+      {user && (
+          <div id="userInfo">{user}</div>
+      )}
     </div>
-  ); 
+  );
 }
 
 const homeStyle = {
   backgroundColor: Theme.colors.beige,
   width: '100vw',
   height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
 };
 
 const Home = () => {
+  const [userNameData, setUserNameData] = React.useState('Megan Ring');
+
   return (
     <div id="home" style={homeStyle}>
-      <Card />
+      <UserProvider value={[userNameData]}>
+        <Card />
+      </UserProvider>
     </div>
   );
 }
